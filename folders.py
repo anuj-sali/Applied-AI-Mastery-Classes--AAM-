1aa00b62da241ceb8f396fddec183e69f347d22c
Sali, Anuj <anuj.sali+accent@accenture.com>
2025-06-11 22:32:23 +0530

    modified folders.py

--- folders.py
+++ folders.py
@@ -8,24 +8,24 @@
 # 6. Python Data Structures.
 # 7. Functions.
 # 8. Modules & Packages.
-import os
-def create_folders(base_path):
-    folders = [
-        "1. Variables & Data Types",
-        "2. Strings in Python",
-        "3. Booleans & Operators",
-        "4. Control Flow",
-        "5. Loops in Python",
-        "6. Python Data Structures",
-        "7. Functions",
-        "8. Modules & Packages"
-    ]
+# import os
+# def create_folders(base_path):
+#     folders = [
+#         "1. Variables & Data Types",
+#         "2. Strings in Python",
+#         "3. Booleans & Operators",
+#         "4. Control Flow",
+#         "5. Loops in Python",
+#         "6. Python Data Structures",
+#         "7. Functions",
+#         "8. Modules & Packages"
+#     ]
     
-    for folder in folders:
-        os.makedirs(os.path.join(base_path, folder), exist_ok=True)
+#     for folder in folders:
+#         os.makedirs(os.path.join(base_path, folder), exist_ok=True)
 # Usage:
-base_path = "1.Core Python Mastery"
-create_folders(base_path)
+# base_path = "1.Core Python Mastery"
+# create_folders(base_path)
 
 # Inside each of these folders (1.Variables & Data Types, 2. Strings in Python etc) folders, create the following folders:
 # 1.1 Course Files
@@ -34,3 +34,5 @@ create_folders(base_path)
 # similarly for other folders:
 # 2.1 Course Files
 # 2.2 Exercise Files
+
+print("Folders created successfully.")
